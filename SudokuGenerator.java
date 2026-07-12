637fc0279cbc08b68bcda7a1be2e02d8a4ebf1d2
Raiyan K <96327738+Amerax@users.noreply.github.com>
2026-07-12 17:51:44 -0400

    Add scramble and swap functions to SudokuGenerator
    actual sudoku genration code complete, randomized board is made, but now implentation of player must be done

--- SudokuGenerator.java
+++ SudokuGenerator.java
@@ -23,8 +23,59 @@ public class SudokuGenerator
              }
           }
     }
-
- public static void printBoard() {
+        
+     public static void scramble() {
+        for (int i = 0; i < 10; i++) {
+            int row = (int)(Math.random() * 3); 
+            int r1 = row * 3 + (int)(Math.random() * 3);
+            int r2 = row * 3 + (int)(Math.random() * 3);
+            swapRows(r1, r2);
+        }
+    
+        
+        for (int i = 0; i < 10; i++) {
+            int col = (int)(Math.random() * 3);
+            int c1 = col * 3 + (int)(Math.random() * 3);
+            int c2 =  col * 3 + (int)(Math.random() * 3);
+            swapCols(c1, c2);
+        }
+    
+        for (int i = 0; i < 10; i++) {
+            int n1 = (int)(Math.random() * 9) + 1;
+            int n2 = (int)(Math.random() * 9) + 1;
+            swapNumbers(n1, n2);
+        }
+    }
+    
+    public static void swapRows(int r1, int r2) {
+        for (int col = 0; col < 9; col++) {
+            int temp = board[r1][col];
+            board[r1][col] = board[r2][col];
+            board[r2][col] = temp;
+        }
+    }
+    
+    public static void swapCols(int c1, int c2) {
+        for (int row = 0; row < 9; row++) {
+            int temp = board[row][c1];
+            board[row][c1] = board[row][c2];
+            board[row][c2] = temp;
+        }
+    }
+    
+    public static void swapNumbers(int n1, int n2) {
+        for (int row = 0; row < 9; row++) {
+            for (int col = 0; col < 9; col++) {
+                if (board[row][col] == n1) {
+                    board[row][col] = n2;
+                } else if (board[row][col] == n2) {
+                    board[row][col] = n1;
+                }
+            }
+        }
+    }
+  
+    public static void printBoard() {
         System.out.println("----------------------");
         int rowCounter = 0;
         for (int i = 0; i < board.length; i++) {
@@ -47,4 +98,7 @@ public class SudokuGenerator
             }
             
         }
- }
+    }
+    
+   
+}
